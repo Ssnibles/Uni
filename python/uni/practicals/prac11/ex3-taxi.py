f5fca612166d475cbe74b6e8f3b202c42475c1ed
Ssnibles <joshua.breite@gmail.com>
2025-05-26 00:13:50 +1200

    25-05-2025

--- python/uni/practicals/prac11/ex3-taxi.py
+++ python/uni/practicals/prac11/ex3-taxi.py
@@ -1,19 +1,16 @@
 import csv
 
 vendor = []
-
-with open("./yellow_tripdata_2015-06.csv", mode="r", newline="") as file:
+with open("./yellow_tripdata_2015-06-1_.csv", mode="r", newline="") as file:
     data = csv.reader(file)
-    heder = next(data)
+    header = next(data)
 
     for line in data:
         # Skip empty lines or malformed lines
         if len(line) < 3:
             continue
-
         vendor_id = int(line[0])
         tip = float(line[15])
-
         if vendor_id == 2:
             vendor.append(tip)
 
