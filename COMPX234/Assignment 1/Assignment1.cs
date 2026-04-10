28cb29c5929866cb678b6721be5d66ff57322248
Ssnibles <joshua.breite@gmail.com>
2026-04-10 19:11:56 +1200

    2026-04-10 19:11

--- COMPX234/Assignment 1/Assignment1.cs
+++ COMPX234/Assignment 1/Assignment1.cs
@@ -1,89 +1,96 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Security.Principal;
 using System.Threading;
 
 class Assignment1
 {
-    // Simulation Initialisation
-    private static readonly int NUM_MACHINES = 50; // Number of machines in the system that issue print requests
-    private static readonly int NUM_PRINTERS = 5; // Number of printers in the system that print requests
-    private static readonly int SIMULATION_TIME = 30;
-    private static readonly int MAX_PRINTER_SLEEP = 3;
-    private static readonly int MAX_MACHINE_SLEEP = 5;
+    // Simulation initialisation
+    private static int NUM_MACHINES = 50;
+    private static int NUM_PRINTERS = 5;
+    private static int SIMULATION_TIME = 30;
+    private static int MAX_PRINTER_SLEEP = 3;
+    private static int MAX_MACHINE_SLEEP = 5;
     private static volatile bool sim_active = true;
 
-    // Create an empty list of print requests
-    printList list = new();
+    // Shared print queue
+    printList list = new printList();
 
-    private static Semaphore Capacity = new Semaphore(5, 5);
-    private static Semaphore Items = new Semaphore(0, 5);
-
-    internal printList List
-    {
-        get => list;
-        set => list = value;
-    }
-
-    // Put any global variables here
+    // Synchronisation tools
+    static Mutex accessLock = new Mutex();
+    static Semaphore availableSpace = new Semaphore(5, 5); // Tracks empty slots in the queue
+    static Semaphore jobsQueue = new Semaphore(0, 5); // Tracks jobs waiting to be printed
 
     public void StartSimulation()
     {
-        // ArrayList to keep for machine and printer threads
-        List<Thread> mThreads = [];
-        List<Thread> pThreads = [];
+        // Lists to hold references to threads
+        List<Thread> mThreads = new List<Thread>();
+        List<Thread> pThreads = new List<Thread>();
 
-        // Create Machine and Printer threads
-        for (int i = 0; i < NUM_PRINTERS; i++)
+        // Create and start machine threads
+        for (int i = 0; i < NUM_MACHINES; i++)
         {
-            Thread t = new Thread(new MachineThread(this, i).SetMachine);
+            int id = i;
+            Thread t = new Thread(() =>
+            {
+                new MachineThread(this, id).InitialiseMachine();
+            });
             mThreads.Add(t);
             t.Start();
         }
 
-        for (int i = 0; i < NUM_MACHINES; i++)
+        // Create and start printer threads
+        for (int i = 0; i < NUM_PRINTERS; i++)
         {
-            Thread t = new Thread(new PrinterThread(this, i).SetPrinter);
+            int id = i;
+            Thread t = new Thread(() =>
+            {
+                new PrinterThread(this, id).InitialisePrinter();
+            });
             pThreads.Add(t);
             t.Start();
         }
 
-        // start the machine threads
-
-        //start the printer threads
-
-        // let the simulation run for some time
+        // Run simulation for set duration
         Sleep(SIMULATION_TIME);
 
-        // finish simulation
+        // Stop simulation loop
         sim_active = false;
 
-        // Wait until all printer threads finish by using the join method
-        foreach (var Thread in pThreads)
+        // Join all threads to ensure clean exit
+        foreach (var t in pThreads)
         {
-            Thread.Join();
+            t.Join();
+        }
+        foreach (var t in mThreads)
+        {
+            t.Join();
         }
     }
 
-    // Printer class
-    public class PrinterThread(Assignment1 parent, int id)
+    public class PrinterThread
     {
-        private readonly Assignment1 outer = parent;
-        private readonly int printerID = id;
+        private readonly Assignment1 outer;
+        private int printerID;
 
-        public void SetPrinter()
+        public PrinterThread(Assignment1 parent, int id)
+        {
+            outer = parent;
+            printerID = id;
+        }
+
+        public void InitialisePrinter()
         {
             while (sim_active)
             {
-                // Simulate printer taking some time to print the document
                 PrinterSleep();
-
-                // Grab the request at the head of the queue and print it
+                PrintDox(printerID);
             }
         }
 
         public void PrinterSleep()
         {
+            // Random delay between 1 and MAX_PRINTER_SLEEP
             int sleepSeconds =
                 1
                 + (int)(new Random(Guid.NewGuid().GetHashCode()).NextDouble() * MAX_PRINTER_SLEEP);
@@ -100,65 +107,86 @@ class Assignment1
 
         public void PrintDox(int printerID)
         {
-            Console.WriteLine("Printer ID:" + printerID + " : now available");
+            while (sim_active)
+            {
+                // Wait for a job to be available in the queue
+                if (jobsQueue.WaitOne(100))
+                {
+                    accessLock.WaitOne(); // Secure exclusive access to queue
 
-            // Write code here
+                    Console.WriteLine("Printer ID:" + printerID + " : now available");
+                    List.queuePrint(List, printerID); // Process the job
 
-            // print from the queue
-            List.queuePrint(List, printerID);
-
-            // Write code here
+                    accessLock.ReleaseMutex();
+                    availableSpace.Release(); // Signal that a slot in the queue is now free
+                    return;
+                }
+            }
         }
 
         private printList List
         {
-            get { return outer.List; }
+            get { return outer.list; }
         }
     }
 
-    // Machine class
-    public class MachineThread(Assignment1 parent, int id)
+    public class MachineThread
     {
-        private readonly Assignment1 outer = parent;
-        private readonly int machineID = id;
+        private readonly Assignment1 outer;
+        private int machineID;
 
-        public void SetMachine()
+        public MachineThread(Assignment1 parent, int id)
+        {
+            outer = parent;
+            machineID = id;
+        }
+
+        public void InitialiseMachine()
         {
             while (sim_active)
             {
-                // machine sleeps for a random amount of time
                 MachineSleep();
 
-                // machine wakes up and sends a print request
-                IsRequestSafe(id);
+                // Request access and send job if space is available
+                if (IsRequestSafe(machineID))
+                {
+                    PrintRequest(machineID);
+                    PostRequest(machineID);
+                }
             }
         }
 
-        public void IsRequestSafe(int id)
+        public bool IsRequestSafe(int id)
         {
             Console.WriteLine("Machine " + id + " Checking availability");
 
-            // Write code here:
-
-            Console.WriteLine("Machine " + id + " will proceed");
+            while (sim_active)
+            {
+                // Check if queue has room
+                if (availableSpace.WaitOne(100))
+                {
+                    accessLock.WaitOne(); // Lock queue for writing
+                    Console.WriteLine("Machine " + id + " will proceed");
+                    return true;
+                }
+            }
+            return false;
         }
 
         public void PrintRequest(int id)
         {
             Console.WriteLine("Machine " + id + " Sending a print request");
+            printDoc doc = new printDoc("My name is machine " + id, id);
 
-            // Build a print document
-            printDoc doc = new("My name is machine " + id, id);
-
-            // Insert it in print queue
-            outer.List = outer.List.queueInsert(outer.List, doc);
+            // Add document to the shared queue
+            outer.list = outer.list.queueInsert(outer.list, doc);
         }
 
         public void PostRequest(int id)
         {
             Console.WriteLine("Machine " + id + " Releasing binary semaphore");
-
-            // Write code here
+            accessLock.ReleaseMutex();
+            jobsQueue.Release(); // Signal that a new job is ready for printing
         }
 
         public void MachineSleep()
@@ -166,6 +194,7 @@ class Assignment1
             int sleepSeconds =
                 1
                 + (int)(new Random(Guid.NewGuid().GetHashCode()).NextDouble() * MAX_MACHINE_SLEEP);
+
             try
             {
                 Thread.Sleep(sleepSeconds * 1000);
